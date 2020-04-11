a8bf0e2323496970c55e26d753d9f0193fd7ef30
Rodrigo Silva <rodrigo_silva92@hotmail.com>
2020-04-10 23:43:41 -0300

    First Commint
    Commit dos estudos iniciais sobre AspNet MVC

---
+++ Introducao/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using Introducao.Models;
+using System.Web.Mvc;
+
+namespace Introducao.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            var pessoa = new Pessoa
+            {
+                PessoaId = 1,
+                Nome = "Rodrigo Silva",
+                Tipo = "Administrador"
+            };
+
+            //Usando ViewData
+            //ViewData["PessoaId"] = pessoa.PessoaId;
+            //ViewData["Nome"] = pessoa.Nome;
+            //ViewData["Tipo"] = pessoa.Tipo;
+
+            //Usando ViewBag
+            //ViewBag.id = pessoa.PessoaId;
+            //ViewBag.nome = pessoa.Nome;
+            //ViewBag.tipo = pessoa.Tipo;
+
+            //return View();
+            return View(pessoa);
+        }
+
+        public ActionResult Contatos()
+        {
+            return View();
+        }
+        //[HttpPost]
+        //uma da maneiras
+        //public ActionResult Lista(int PessoaId, string Nome, string Tipo)
+        //{
+        //    ViewData["PessoaId"] = PessoaId;
+        //    ViewData["Nome"] = Nome;
+        //    ViewData["Tipo"] = Tipo;
+
+        //    return View();
+        //}
+
+            //view tipada
+        [HttpPost]
+        public ActionResult Lista(Pessoa pessoa)
+        {
+            ViewData["PessoaId"] = pessoa.PessoaId;
+            ViewData["Nome"] = pessoa.Nome;
+            ViewData["Tipo"] = pessoa.Tipo;
+
+            return View();
+        }
+    }
+}
