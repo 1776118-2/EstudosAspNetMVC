3df585fcd9da17dd85f3143e0d4560f6a34052e6
Rodrigo Silva <rodrigo_silva92@hotmail.com>
2020-04-20 02:29:05 -0300

    Finalizado Projeto Introdução;
    - Finalizado Projeto Introdução;

--- Introducao/Controllers/UsuarioController.cs
+++ Introducao/Controllers/UsuarioController.cs
@@ -1,6 +1,7 @@
 ﻿using Introducao.Models;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -19,7 +20,22 @@ namespace Introducao.Controllers
         //VALIDAÇÂO DE USUÁRIO
         [HttpPost]
         public ActionResult Usuario(Usuario usuario)
-        {
+        {   //forma de validação para casos simples!
+            ////Exemplo 1 de validação:
+            //if(string.IsNullOrEmpty(usuario.Nome))
+            //{
+            //    ModelState.AddModelError("Nome", "O campo nome é obrigatório");
+            //}
+
+            ////Exemplo 2 de validação:
+            //if(usuario.Senha != usuario.ConfirmarSenha)
+            //{
+            //    //Não coloca nome de propriedade, pois o validateSumary gera a msg de erro automático
+            //    ModelState.AddModelError("", "As senhas são diferentes");
+            //}
+
+
+
             if(ModelState.IsValid)
             {
                 //Se o usuario for válido redireciona para a View
@@ -31,6 +47,19 @@ namespace Introducao.Controllers
         public ActionResult Resultado(Usuario usuario)
         {
             return View(usuario);
+        }
+
+        public ActionResult LoginUnico(String login)
+        {
+            //Banco de dados fictício com 3 registros
+            var bdExemplo = new Collection<string>
+            {
+                "Rodrigo",
+                "Silva",
+                "Santos"
+            };
+            //se o registro for diferente que o usuário inserir
+            return Json(bdExemplo.All(x => x.ToLower() != login.ToLower()), JsonRequestBehavior.AllowGet);
         }
     }
 }
--- Introducao/Models/Usuario.cs
+++ Introducao/Models/Usuario.cs
@@ -1,18 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace Introducao.Models
 {
     public class Usuario
-    {        
+    {   
+        //Exemplos de Validação na cs
+        [Required(ErrorMessage = "O nome é obrigatório")]
         public string Nome { get; set; }
+
+        [StringLength(50, MinimumLength = 5, ErrorMessage = "Insira uma informação de 5 a 50 caracteres" )]
         public string Observacoes { get; set; }
+
+        //Exemplo de validação com Range :
+        [Range(18,70, ErrorMessage = "A sua idade tem que estar entre 18 e70 anos")]
         public int Idade { get; set; }
+        
+        //Exemplo de validação com RegularExpression
+        [RegularExpression(@"\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*", ErrorMessage = "Digite um e-mail válido")]
         public String Email { get; set; }
+
+        //validações do login
+        [RegularExpression(@"[a-zA-Z]{5,15}", ErrorMessage = "Somente letras e de 5 à 15 caracteres")]
+        [Required(ErrorMessage = "O login é obrigatório")]
+        [Remote("LoginUnico", "Usuario", ErrorMessage = "Este login já existe.")]
         public String Login { get; set; }
+
+        [Required(ErrorMessage = "A senha é obrigatório")]
         public String Senha { get; set; }
+
+        //Exemplo de comparação de senha
+        [System.ComponentModel.DataAnnotations.Compare("Senha", ErrorMessage ="As senhas não se coincidem")]
         public String ConfirmarSenha { get; set; }
     }
 }
